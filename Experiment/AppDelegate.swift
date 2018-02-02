86abce88641df9b556efe4b322ac3712ddce6ac0
Michael Lascarides <mlascarides@gmail.com>
2018-02-02 16:44:07 +1300

    Initial Commit

---
+++ Experiment/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Experiment
+//
+//  Created by Michael NLNZ on 2/02/18.
+//  Copyright © 2018 Digital New Zealand. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
